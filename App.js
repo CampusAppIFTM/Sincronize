d9c583faddeacb0479adb2e710835045ce2b6ad9
Lara Campos <lara.goveias@estudante.iftm.edu.br>
2026-08-04 14:23:26 -0300

    Mudei o título do botão

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ function Home({ navigation }) {
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home screen</Text>
       <Button
-        title="Go to Profile"
+        title="Tela inicial do app"
         onPress={() => navigation.navigate('Profile')}
       />
     </View>
